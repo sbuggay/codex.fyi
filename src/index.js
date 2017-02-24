36fb76dc55a10cb4425ced3dd15bf361b49d514c
Devan Buggay <debuggay@microsoft.com>
2017-02-24 13:19:54 -0800

    Implementation of post and create page

--- src/index.js
+++ src/index.js
@@ -6,34 +6,38 @@ let ObjectId = mongodb.ObjectId;
 let db;
 
 app.use(bodyParser.json());
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-	res.sendfile(__dirname + "/public/index.html");
+	res.sendFile(__dirname + "/public/index.html");
 });
 
 app.get("/about", (req, res) => {
-	res.sendfile(__dirname + "/public/about.html");
+	res.sendFile(__dirname + "/public/about.html");
 });
 
 app.get("/:id.json", (req, res) => {
-	db.collection('posts').findOne({ _id: req.params.id }, (err, result) => {
+	db.collection('posts').findOne(ObjectId(req.params.id) , (err, result) => {
 		if (err) return res.send({ status: "error", error: err });
+		console.log(req.params.id);
+		console.log(result);
 		res.send(result);
 	})
 });
 
 app.get("/:id", (req, res) => {
-	res.sendfile(__dirname + "/public/post.html");
+	res.sendFile(__dirname + "/public/post.html");
 });
 
 app.post("/", (req, res) => {
+	console.log(req.body);
 	db.collection('posts').save(req.body, (err, result) => {
 		if (err) return res.send({ status: "error", error: err });
 		res.send({ status: "success", id: result.ops[0]["_id"] });
 	})
 });
 
-mongodb.MongoClient.connect("<connection_uri_here>", (err, database) => {
+mongodb.MongoClient.connect("<mongodb_uri>", (err, database) => {
 	if (err) return console.error(err)
 	db = database;
 	app.listen(8080);
